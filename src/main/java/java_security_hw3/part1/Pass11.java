46a560e2500e844717a8c5aa50d869236904983c
dwilso95 <dwilso95@IP-C6136741.corp.amazonworkspaces.com>
2017-10-19 16:34:33 -0400

    Updated Pass11 docs, modified class file, and screenshot.

--- src/main/java/java_security_hw3/part1/Pass11.java
+++ src/main/java/java_security_hw3/part1/Pass11.java
@@ -1,7 +1,8 @@
 package java_security_hw3.part1;
 
 /**
- * This class does nothing. It will be manipulated to have an incorrect magic number
+ * This class does nothing. It will be manipulated to have a corrupt constants
+ * section.
  */
 public class Pass11 {
 
